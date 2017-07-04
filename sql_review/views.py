13f3fd037f88a82f1a31b24ce34cc27ce783d6d2
吴亚楠 <wuyanan@wuyanandeMBP.lan>
2017-07-05 00:24:20 +0800

    Initial commit

---
+++ sql_review/views.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import json
+import os
+import MySQLdb
+from MySQLdb.constants.CLIENT import MULTI_STATEMENTS, MULTI_RESULTS
+from django.http.response import HttpResponse
+
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def review(request):
+    file_object = open('public/sql_review.txt')
+    try:
+        all_the_text = file_object.read()
+    finally:
+        file_object.close()
+    try:
+        conn = MySQLdb.connect(host='192.168.0.249', user='', passwd='', db='', port=6666,
+                               client_flag=MULTI_STATEMENTS | MULTI_RESULTS)
+        cur = conn.cursor()
+        ret = cur.execute(all_the_text)
+        num_fields = len(cur.description)
+        field_names = [i[0] for i in cur.description]
+        result = cur.fetchall()
+        cur.close()
+        conn.close()
+        data = {
+            'field_names': field_names,
+            'result': result
+        }
+        print(result)
+        return render(request, 'corporate/blank.html', data)
+    except MySQLdb.Error as e:
+        print('Mysql Error %d: %s' % (e.args[0], e.args[1]))
---
+++ statistics/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http.response import HttpResponse
+
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    print('sss')
+    return HttpResponse('hello django', status=200)
+
